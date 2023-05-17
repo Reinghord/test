4ff8164394754a09c10fff594354c94fb23a372e
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2023-05-17 21:46:41 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './Transactions/TransactionHistory';
+// import { Profile } from './Profile/Profile';
+// import { Statistics } from './Statistics/Statistics';
+// import { FriendList } from './FriendList/FriendList';
+// import { TransactionHistory } from './Transactions/TransactionHistory';
 
-import user from './Profile/user.json';
-import data from './Statistics/data.json';
-import friends from './FriendList/friends.json';
-import transactions from './Transactions/transaction.json';
+// import user from './Profile/user.json';
+// import data from './Statistics/data.json';
+// import friends from './FriendList/friends.json';
+// import transactions from './Transactions/transaction.json';
 
 export const App = () => {
   return (
     <div>
-      <Profile
+      {/* <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
@@ -20,7 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
+      <TransactionHistory items={transactions} />; */}
     </div>
   );
 };
