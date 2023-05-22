e3b8a715691a8e34c31e21c3169ac612a9ef13ec
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2023-05-22 18:26:47 +0300

    m

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -13,10 +13,8 @@ import {
 } from './ContactsForm.styled';
 
 const INITIAL_STATE = {
-  contacts: [],
   name: '',
   number: '',
-  type: '',
 };
 
 class ContactsForm extends Component {
@@ -25,20 +23,15 @@ class ContactsForm extends Component {
   onFormSubmit = event => {
     event.preventDefault();
     const { name, number } = this.state;
-    const { contacts } = this.props;
-    console.log(this.props);
     try {
-        const existingContact = contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase()
-      );
-      if (existingContact) {
-        return alert(`${name} has already in contacts.`);
-      }
       this.props.addContact({ id: nanoid(), name: name, number: number });
       this.setState(INITIAL_STATE);
     } catch (ReferenceError) {
         console.log("An ReferenceError error occured");
     }
+    console.log(this.props);
+    this.props.addContact({ id: nanoid(), name: name, number: number });
+    this.setState(INITIAL_STATE);
   };
 
   onInputChange = ({ target: { name, value } }) => {
@@ -68,7 +61,7 @@ class ContactsForm extends Component {
               placeholder='name'
               onChange={this.onInputChange}
               value={name}
-              pattern="/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
             />
@@ -82,7 +75,7 @@ class ContactsForm extends Component {
               placeholder='tel number'
               onChange={this.formatNumber}
               value={number}
-              pattern="\+?\d{1,4}[-.\s]?\d{1,3}[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
               required
             />
